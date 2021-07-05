45f614badd89e1810dc0ef9151f107d599a0acd5
brwnghst77 <IZ@MacBook-Air.socal.rr.com>
2021-07-04 21:15:42 -0700

    est repo

---
+++ develop/script.js
@@ -0,0 +1,26 @@
+// Assignment code here
+
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+let btnEl = document.querySelector('#btn')
+let clickHandler = function() {
+    alert('Button was pressed.')
+};
+
+btnEl.addEventListener('click', function() {
+    alert('Button was pressed.');
+});
